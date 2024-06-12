fdec35f4b1f686555c6a0dd4d6192858b61ec959
efraimG21 <efraimg621@icloud.com>
2024-06-12 20:36:37 +0300

    commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./components/pages/home-page/home-page.component";
+import {SignOnPageComponent} from "./components/pages/sign-on-page/sign-on-page.component";
+import {ChatPageComponent} from "./components/pages/chat-page/chat-page.component";
+import {NotFoundPageComponent} from "./components/pages/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent, title: 'home'},
+  {path: 'sign-on', component: SignOnPageComponent, title: 'sign on'},
+  {path: 'chat/:uid', component: ChatPageComponent, title: 'chat'},
+  {path: '**', component: NotFoundPageComponent, title: '404 - not found'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
